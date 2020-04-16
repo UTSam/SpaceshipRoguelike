ef569f812fdbc97c8818bbe646c51b129d367ba6
Robbert Ritsema <RobbertRitsema@protonmail.com>
2020-04-16 11:16:09 +0200

    Initial porject

---
+++ Spaceship Roguelike/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private PlayerMovement playerMovement;
+    void Start()
+    {
+        playerMovement = new PlayerMovement(rb);
+    }
+    void Update()
+    {
+        playerMovement.Update();
+    }
+
+    void FixedUpdate()
+    {
+        playerMovement.FixedUpdate();
+    }
+}
---
+++ Spaceship Roguelike/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    class PlayerMovement
+    {
+        public float moveSpeed = 5f;
+        private Rigidbody2D rb;
+
+        Vector2 movement;
+
+        public PlayerMovement(Rigidbody2D rb)
+        {
+            this.rb = rb;
+        }
+
+        internal void Update()
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
+
+        internal void FixedUpdate()
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
