3737c8c1f9a1e2b116a16d6164d8a6d5a9606e2f
Jacco Douma <jaccodouma@gmail.com>
2020-04-21 12:50:31 +0200

    Added basic room creation
    Added ability to create a room and save it as a prefab.

---
+++ Assets/Scripts/Rooms/Room.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Room : MonoBehaviour
+{
+    public TileBase[] tiles;
+    public Vector3Int[] positions;
+
+    public Tilemap tilemap; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateRoom(Vector3Int pos)
+    {
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            tilemap.SetTile(positions[i] + pos, tiles[i]);
+        }
+    }
+}
---
+++ Assets/Scripts/Rooms/Room_Generate.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Room_Generate : MonoBehaviour
+{
+    public BoundsInt area;
+    public Tilemap tilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjectFromTilemap(tilemap);
+    }
+
+    private void ObjectFromTilemap(Tilemap tilemap)
+    {
+        List<TileBase> tiles = new List<TileBase>();
+        List<Vector3Int> pos = new List<Vector3Int>();
+
+        // Add tiles to list
+        for (int x = tilemap.cellBounds.xMin; x < tilemap.cellBounds.xMax; x++)
+        {
+            for (int y = tilemap.cellBounds.yMin; y < tilemap.cellBounds.yMax; y++)
+            {
+                Tile tile = tilemap.GetTile<Tile>(new Vector3Int(x, y, 0));
+
+                if (tile != null)
+                {
+                    tiles.Add(tile);
+                    pos.Add(new Vector3Int(x, y, 0));
+
+                    Debug.Log(tile.sprite.name);
+                }
+            }
+        }
+
+        // Create empty gameobject, add "Tile" script and populate values
+        GameObject newObj = new GameObject("Room");
+        newObj.AddComponent<Room>();
+        newObj.GetComponent<Room>().tiles = tiles.ToArray();
+        newObj.GetComponent<Room>().positions = pos.ToArray();
+        newObj.GetComponent<Room>().tilemap = tilemap;
+
+        tilemap.ClearAllTiles();
+
+        newObj.GetComponent<Room>().CreateRoom(new Vector3Int(0,0,0));
+    } 
+}
