8d153144852a45f6ad1d5f472eb1ffeec2755697
ThibautRousselet <38785880+ThibautRousselet@users.noreply.github.com>
2020-04-22 16:37:43 +0200

    Revert "Revert "Merge branch 'Feature_Basic_Entities' of https://github.com/UTSam/SpaceshipRoguelike into Feature_Basic_Entities""
    This reverts commit 015626635fe4fb7a64f106bc85351c8bbb190389.

---
+++ Assets/Scripts/Rooms/Room.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Room : MonoBehaviour
+{
+    public Tile[] tiles;
+    public Vector3Int[] positions;
+
+    public Tilemap tilemap; 
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        tilemap = GameObject.Find("Tilemap_Walls").GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateRoom(Vector3Int pos)
+    {
+        if(tilemap == null)
+            return;
+
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            tilemap.SetTile(positions[i] + pos, tiles[i]);
+        }
+    }
+
+    public List<Vector3Int> GetDoorPositions()
+    {
+        List<Vector3Int> tempPos = new List<Vector3Int>();
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            if (tiles[i].sprite.name == "Door")
+                tempPos.Add(positions[i]);
+        }
+
+        return tempPos;
+    }
+}
